4285bb44c8fa6da9e94ef709ccb8faea0d2da1c5
TheNutsOnYourChin <1459840399@qq.com>
2020-01-23 22:33:21 +0800

    eureka服务注册中心

---
+++ boki-eureka/src/test/java/com/boki/bokieureka/BokiEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boki.bokieureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokiEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
